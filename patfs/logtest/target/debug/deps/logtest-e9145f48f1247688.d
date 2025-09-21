89d4f9d2c0ab2b7325f3a1859a964a5bcd15d7e4
Alexander Carlisi <alexandercarlisi0@gmail.com>
2025-09-21 19:02:31 -0400

    Remove target files since its in the gitignore

--- patfs/logtest/target/debug/deps/logtest-e9145f48f1247688.d
+++
@@ -1,5 +0,0 @@
-/home/lilroo/code/PattyCakes-Flight-Computer/patfs/logtest/target/debug/deps/logtest-e9145f48f1247688.d: src/main.rs
-
-/home/lilroo/code/PattyCakes-Flight-Computer/patfs/logtest/target/debug/deps/liblogtest-e9145f48f1247688.rmeta: src/main.rs
-
-src/main.rs:
--- patfs/logtest/target/debug/logtest.d
+++
@@ -1,1 +0,0 @@
-/home/lilroo/code/PattyCakes-Flight-Computer/patfs/logtest/target/debug/logtest: /home/lilroo/code/PattyCakes-Flight-Computer/patfs/logtest/src/main.rs
