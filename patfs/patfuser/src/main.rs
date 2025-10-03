446a5cb8e95d74de6d49c5f57f61f980334a45a9
Alexander Carlisi <alexandercarlisi0@gmail.com>
2025-10-03 12:18:12 -0400

    Overhaul the PatFuser code
    Basically none of it even actually worked, this might we shall see.

--- patfs/patfuser/src/main.rs
+++ patfs/patfuser/src/main.rs
@@ -1,10 +1,11 @@
+use clap::builder::IntoResettable;
 /**
  * Modified fuser example "hello.rs"
  *
  */
 
 // FUSER includes
-use clap::{Arg, ArgAction, Command, crate_version};
+use clap::{Arg, ArgAction, Command};
 use fuser::{
     FileAttr, FileType, Filesystem, MountOption, ReplyAttr, ReplyData, ReplyDirectory, ReplyEntry,
     Request,
@@ -12,11 +13,11 @@ use fuser::{
 use libc::ENOENT;
 use std::ffi::OsStr;
 use std::time::{Duration, UNIX_EPOCH};
-use chrono::{NaiveDateTime, Local, TimeZone};
+use chrono::{DateTime, Local, TimeZone};
 
 // SD includes
 use std::fs::OpenOptions;
-use std::io::{Read, Seek, SeekFrom, Write};
+use std::io::{Read, Seek, SeekFrom};
 
 // FUSER Consts
 const TTL: Duration = Duration::from_secs(1); // 1 second
@@ -24,37 +25,57 @@ const FS_NAME: &str = "PatFS";
 
 // SD Consts
 const BLOCK_SIZE: usize = 512; // BYTES
-const META_DATA_SIZE: usize = 26;
+const META_DATA_SIZE: usize = 8; // BYTES
 
+/// Signaling the beginning of a Log, and subsequent Metadata -> flight states
+const LOG_BEGIN_SEQUENCE: &str = "$$$$$$$$";
 
-struct Log<'a> {
-    stop_seq: &'a str, 
+/// This allows the size of a state to be variable
+/// which is nice for future updates where they may shrink or have more data
+const LOG_STATE_SEQUENCE: &str = "&&&&&&&&";
+
+const SD_PATH: &str = "mock_sd.img";
+
+struct LogFile {
     ino: u64,
-    size: usize,
     epoch: i64,
-    filename: String
+    filename: String,
+    file_size: usize // metasize + all_states
 }
-impl Log {
-    fn from_slice(ino: u64, slice: [u8; usize]) -> Log {
-        let stop_seq = slice[0..8];
-        let epoch: i64 = slice[9..17];
-        let size: usize = slice[18..20];
-        let dt: DateTime<Local> = DateTime::from_timestamp_secs(log.epoch);
-        let filename: String = dt.format("%m/%d/%Y %H:%M:%S").to_string();
-        return Log {
-            stop_seq,
+
+impl LogFile {
+    
+    fn check_seq<'a>(slice: &'a [u8], sequence: &str) -> bool {
+        let seq = str::from_utf8(&slice);
+        return seq.is_ok_and(|x| x == sequence);
+    }
+
+    fn from_slice<'a>(ino: u64, slice: &'a [u8]) -> LogFile {
+        let epoch: i64 = i64::from_be_bytes(slice[..8].try_into().unwrap());
+        let dt: DateTime<Local> = Local.timestamp_opt(epoch, 0).unwrap();
+        let filename: String = dt.format("%m/%d/%Y-%H:%M:%S").to_string();
+        
+        let mut i: usize = META_DATA_SIZE; // Skip metadata
+        while i < slice.len() {
+            if i+8 < slice.len() && LogFile::check_seq(&slice[i..i+8], LOG_BEGIN_SEQUENCE) {
+                break;
+            }
+            i += 1;
+        }
+
+        return LogFile {
             ino,
-            size,
             epoch,
-            filename
+            filename,
+            file_size: i
         }
     }
 
     fn to_fileattr(&self) -> FileAttr {
         return FileAttr {
             ino: self.ino,
-            size: self.size,
-            blocks: (self.size + BLOCK_SIZE - 1) / BLOCK_SIZE,
+            size: self.file_size as u64,
+            blocks: ((self.file_size + BLOCK_SIZE - 1) / BLOCK_SIZE) as u64,
             atime: UNIX_EPOCH,
             mtime: UNIX_EPOCH,
             ctime: UNIX_EPOCH,
@@ -66,37 +87,43 @@ impl Log {
             gid: 20,
             rdev: 0,
             flags: 0,
-            blksize: BLOCK_SIZE
+            blksize: BLOCK_SIZE as u32
         };
     }
-}
 
-static LOGS: Vec<Log> = Vec::new();
+    fn get_logfiles() -> Option<Vec<LogFile>> {
+        let mut logs: Vec<LogFile> = Vec::new();
+        let mut card = OpenOptions::new()
+            .write(false)
+            .create(false)
+            .read(true)
+            .open(SD_PATH)
+            .ok()?; // TODO: proper path?
+        card.seek(SeekFrom::Start(0)).ok()?;
+        let mut buffer: Vec<u8> = Vec::new();
+        card.read_to_end(&mut buffer).ok()?;
 
-fn populate_logs() {
-    let mut card = OpenOptions::new()
-        .write(false)
-        .create(false)
-        .read(true)
-        .open("mock_sd.img")?; // TODO: proper path?
-                               // 
-    sd_mock.seek(SeekFrom::Start(0))?;
-    let mut buffer: Vec<u8> = Vec::new();
-    sd_mock.read_to_end(&mut buffer)?;
-    let mut ino = 2 as u64; // 1 = logs dir
-    let mut i = 0;
-    while i < buffer.len() {
-        if (i+8) < buffer.len() {
-            let charseq = std::str::from_utf8(&buffer[i..i+8]);
-            if charseq.is_ok_and(|x| x == LOG_BEGIN_SEQUENCE) {
-                LOGS.push(Log::from_slice(ino, buffer[i..]));
-                ino += 1;
-                i += 8;
-            }
+        let mut i = 0;
+        let mut ino = 2; // 1 = logs dir
+        while i < buffer.len() {
+            let log: LogFile =  LogFile::from_slice(ino, &buffer[i..]);
+            i += log.file_size;
+            logs.push(log);
+            ino += 1;
         }
+        
+        return Some(logs);
+    }
+    
+    fn get_fileattrs() -> Option<Vec<FileAttr>> {
+        let logfiles: Vec<LogFile> = LogFile::get_logfiles().unwrap();
+        let mut fileattrs: Vec<FileAttr> = Vec::new();
+        for log in logfiles {
+            fileattrs.push(log.to_fileattr());
+        }
+        return Some(fileattrs);
     }
 }
-
 
 /**
  * Linux Permission levels
@@ -127,51 +154,31 @@ const LOGS_DIR_ATTR: FileAttr = FileAttr {
 };
 
 
-// TODO: ATTRIBUTES OF LOG FILE
-// const HELLO_TXT_ATTR: FileAttr = FileAttr {
-//     ino: 2,
-//     size: 13,
-//     blocks: 1,
-//     atime: UNIX_EPOCH, // 1970-01-01 00:00:00
-//     mtime: UNIX_EPOCH,
-//     ctime: UNIX_EPOCH,
-//     crtime: UNIX_EPOCH,
-//     kind: FileType::RegularFile,
-//     perm: 0o644, // [readwrite, read, read]
-//     nlink: 1,
-//     uid: 501,
-//     gid: 20,
-//     rdev: 0,
-//     flags: 0,
-//     blksize: 512,
-// };
-
-
 struct PatFS;
 
 impl Filesystem for PatFS {
     fn lookup(&mut self, _req: &Request, parent: u64, name: &OsStr, replyentry: ReplyEntry) {
-        let mut found: bool = false;
-        for log in LOGS {
-            if name.to_str() == log.filename {
-                replyentry.entry(&TTL, log::to_fileattr(), 0);
-                found = true;
-                break;
+        let logfiles = LogFile::get_logfiles().unwrap(); // TODO: error handle?
+        for log in logfiles {
+            if name.to_str() == Some(log.filename.as_str()) {
+                replyentry.entry(&TTL, &log.to_fileattr(), 0);
+                return;
             }
         }
-        if !found {
-            replyentry.error(ENOENT);
-        }
+        replyentry.error(ENOENT);
     }
-
-    fn getattr(&mut self, _req: &Request, ino: u64, _fh: Option<u64>, replyattr: ReplyAttr) {
+    
+    /** The FUSER docs specify a param that was removed _fh :) */
+    fn getattr(&mut self, _req: &Request, ino: u64, replyattr: ReplyAttr) {
         if ino == 1 {
             replyattr.attr(&TTL, &LOGS_DIR_ATTR);
-        } else if ino > LOGS.len() - 1 {
-            replyattr.error(ENOENT);
         } else {
-            let log: Log = LOGS[ino-1];
-            replyattr.attr(&TTL, &log::to_fileattr());
+            let logattrs = LogFile::get_fileattrs().unwrap(); // TODO: error handle?
+            if ino-2 > (logattrs.len() - 1) as u64 {
+                replyattr.error(ENOENT);
+            } else {
+                replyattr.attr(&TTL, &logattrs[(ino as usize)-2]);
+            }
         }
     }
 
@@ -186,21 +193,26 @@ impl Filesystem for PatFS {
         _lock: Option<u64>,
         replydata: ReplyData,
     ) {
-        if ino >= 2 && ino < LOGS.len() { // read_from_splice or something from SD, and just return that type shit
-            // replydata.data(&HELLO_TXT_CONTENT.as_bytes()[offset as usize..]);
-            let leading_logs: [Log; usize] = LOGS[..ino-1];
+        let logfiles = LogFile::get_logfiles().unwrap();
+        if ino >= 2 && ino-2 < logfiles.len() as u64 {
             let mut card = OpenOptions::new()
                 .write(false)
                 .create(false)
                 .read(true)
-                .open("mock_sd.img")?; // TODO: proper path?
+                .open(SD_PATH)
+                .unwrap(); // TODO: proper path?
 
-            let mut seekto: u32 = 0;
-            for log in leading_logs {
-                seekto += log.size + META_DATA_SIZE;
+            let mut seekto = 0;
+            let mut logi = 0;
+            while logi < ino-2 {
+                seekto += logfiles[logi as usize].file_size;
+                logi += 1;
             }
-            let bytes = sd_mock.read_at(seekto+offset, _size)?;
-            replydata.data(&bytes);
+            seekto += offset as usize;
+            card.seek(SeekFrom::Start(seekto as u64)).unwrap(); // TODO:
+            let mut buffer: Vec<u8> = Vec::with_capacity(_size as usize);
+            card.read_exact(&mut buffer).unwrap();
+            replydata.data(&buffer);
         } else {
             replydata.error(ENOENT);
         }
@@ -219,16 +231,15 @@ impl Filesystem for PatFS {
             return;
         }
 
-        let entries = vec![
+        let mut entries = vec![
             // INO, FILETYPE, NAME
-            (1, FileType::Directory, "."),
-            (1, FileType::Directory, ".."),
+            (1, FileType::Directory, ".".to_string()),
+            (1, FileType::Directory, "..".to_string()),
         ];
         
-        LOGS.clear();
-        populate_logs();
-        for log in LOGS {
-            entries.push((log.ino, FileType::RegularFile, String::as_str(log.filename)));
+        let logfiles = LogFile::get_logfiles().unwrap();
+        for log in logfiles {
+            entries.push((log.ino, FileType::RegularFile, log.filename));
         }
 
         for (i, entry) in entries.into_iter().enumerate().skip(offset as usize) {
@@ -243,7 +254,7 @@ impl Filesystem for PatFS {
 
 fn main() {
     let matches = Command::new("FuserPatFSMount")
-        .version(crate_version!())
+        .version("0")
         .author("Alexander Carlisi")
         .arg(
             Arg::new("MOUNT_POINT")
@@ -265,7 +276,7 @@ fn main() {
         )
         .get_matches(); // parse the CLI args
     
-    env_logger::init();
+    // env_logger::init();
     let mountpoint = matches.get_one::<String>("MOUNT_POINT").unwrap();
     
     // Mount Options, get passed to the OS to deal with.
